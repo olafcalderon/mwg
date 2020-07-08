9f679153b8c1756dffa76589f8f23b1a648e345a
Erick Calder <e@arix.com>
2020-07-08 16:23:16 -0700

    turned debugging on

--- wp-config.php
+++ wp-config.php
@@ -77,7 +77,8 @@ $table_prefix = 'wppc_';
  *
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
-define( 'WP_DEBUG', false );
+define( 'WP_DEBUG', true );
+define( 'WP_DEBUG_LOG', true );
 
 /* That's all, stop editing! Happy publishing. */
 
